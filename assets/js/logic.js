bdd4e36f0f372451152daad9a249dbdd9e396082
joelgogo15-ZabuYz <joelgogo15@gmail.com>
2024-01-16 17:45:09 +0000

    Two arrays for the questions and choices

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,29 +1,48 @@
 var startBtn = document.getElementById("start");
 var subut = document.getElementById("submit");
-var secondsLeft = 75;
+var secondsLeft = 76;
 var timerInterval;  
+var jsquestions = document.querySelector("questions");
+var choicesjs = document.querySelector("choices")
 
 function setTime() {
     timerInterval = setInterval(function () {
         secondsLeft--;
         document.getElementById("time").textContent = secondsLeft ;
-
-        if (secondsLeft === 0) {
+        if (secondsLeft < 0) {
             clearInterval(timerInterval);
-            
+            alert("Run out of time!");
         }
     }, 1000);
 }
 
+console.log(questions.length)
+function arrquestions() {
+    jsquestions.textContent = "";
+    for (var i = 0; i < questions.length; i++) {
+        var questLi = document.createElement("li");
+        questLi.textContent = questions[i];
+        jsquestions.append(questLi);
+    }
+}
+arrquestions();
+
 startBtn.addEventListener("click", function () {
     setTime();
     document.getElementById("start-screen").classList.add("hide");
+
+    
     document.getElementById("questions").classList.remove("hide");
-  
+
+  document.getElementById("question-title")
 });
+
+console.log()
 
 subut.addEventListener("click", function () {
     clearInterval(timerInterval); 
 });
 
-questions.textContent
+console.log(questions[2])
+console.log(choices[2])
+console.log(correctAnswers[2])
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,52 +1,42 @@
 var questions = [
-    {
-        question: "Which of these is a type in JavaScript",
-        answers: {
-          1: "Language",
-          2: "Word",
-          3: "Float",
-          4: "String"
-        },
-        correctAnswer: "4"
-    },
-    {
-        question: "What does isNaN mean",
-        answers: {
-          1: "The value is a number",
-          2: "The value is a string",
-          3: "The value is a bool",
-          4: "The value is undefined"
-        },
-        correctAnswer: "1"
-    }
-    , {
-        question: "What does the 'parseInt()' function do in JavaScript?",
-        answers: {
-            1: "Converts a string to a floating-point number",
-            2: "Converts a string to an integer",
-            3: "Converts a number to a string",
-            4: "Returns the length of a string"
-        },
-        correctAnswer: "2"
-    },
-    {
-        question: "Which of the following functions is used to print output to the console in JavaScript?",
-        answers: {
-            1: "console.log()",
-            2: "print()",
-            3: "display()",
-            4: "output()"
-        },
-        correctAnswer: "1"
-    } ,
-    {
-        question: "What does the term 'API' stand for in the context of software development?",
-        answers: {
-            1: "Advanced Programming Interface",
-            2: "Application Programming Interface",
-            3: "Automated Protocol Integration",
-            4: "Adaptive Programming Interface"
-        },
-        correctAnswer: 2
-    },
+    "Which of these is a type in JavaScript",
+    "What does isNaN mean",
+    "What does the 'parseInt()' function do in JavaScript?",
+    "Which of the following functions is used to print output to the console in JavaScript?",
+    "What does the term 'API' stand for in the context of software development?"
 ];
+
+var choices = [
+    {
+        1: "Language",
+        2: "Word",
+        3: "Float",
+        4: "String"
+    },
+    {
+        1: "The value is a number",
+        2: "The value is a string",
+        3: "The value is a bool",
+        4: "The value is undefined"
+    },
+    {
+        1: "Converts a string to a floating-point number",
+        2: "Converts a string to an integer",
+        3: "Converts a number to a string",
+        4: "Returns the length of a string"
+    },
+    {
+        1: "console.log()",
+        2: "print()",
+        3: "display()",
+        4: "output()"
+    },
+    {
+        1: "Advanced Programming Interface",
+        2: "Application Programming Interface",
+        3: "Automated Protocol Integration",
+        4: "Adaptive Programming Interface"
+    }
+];
+
+var correctAnswers = ["4", "1", "2", "1", "2"];
